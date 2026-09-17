7d4a7ccb18d26fd14bb6eaa89a6331c227f24348
Devasri A <devasri30anandhababu@gmail.com>
2026-09-17 09:35:18 +0530

    Implement stack using queues with basic operations

---
+++ Implement Stack using Queues.java
@@ -0,0 +1,31 @@
+class MyStack {
+
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+
+        int size = q.size();
+
+        q.add(x);
+
+        for (int i = 0; i < size; i++) {
+            q.add(q.poll());
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
