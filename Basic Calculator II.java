1b2d6a2cbd686520a00f80b2ebe88d7cc062bd01
Devasri A <devasri30anandhababu@gmail.com>
2026-09-26 22:57:57 +0530

    Update Basic Calculator II.java

--- Basic Calculator II.java
+++ Basic Calculator II.java
@@ -1,19 +1,14 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> stack = new Stack<>();
-
         int num = 0;
         char op = '+';
-
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-
             if (Character.isDigit(ch)) {
                 num = num * 10 + (ch - '0');
             }
-
             if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
-
                 if (op == '+') {
                     stack.push(num);
                 } else if (op == '-') {
@@ -23,18 +18,14 @@ class Solution {
                 } else {
                     stack.push(stack.pop() / num);
                 }
-
                 op = ch;
                 num = 0;
             }
         }
-
         int ans = 0;
-
         while (!stack.isEmpty()) {
             ans += stack.pop();
         }
-
         return ans;
     }
 }
